0af622a26c7f3bb8a6d3737f7ac113968b272862
FishYuCui <1689906778@qq.com>
2021-08-10 11:39:42 +0800

    feat: mommyfier

--- src/Mommifier.js
+++ src/Mommifier.js
@@ -1,5 +1,29 @@
 class Mommifier {
+    constructor(str) {
+        this.str = str
+    }
+    mommifier = () => {
+        const vowels = ['a', 'e', 'u', 'i', 'o']
+        if (this.str === "") {
+            return this.str
+        }
+        const strLength = this.str.length;
+        let newArray = this.str.split("");
+        let number = 0
+        let filterArray = newArray.map(element => {
+            if (vowels.includes(element)) {
+                number++;
+                return "1"
+            }
+            return element
+        });
+        if (number / strLength < 0.3) {
+            return this.str
+        } else {
+            return Array.from(new Set(filterArray)).join("").replace(/1/gi, 'mommy')
+        }
 
+    }
 }
 
 export default Mommifier;
--- src/test/Mommifier.test.js
+++ src/test/Mommifier.test.js
@@ -6,4 +6,34 @@ describe('Mommifier Test', () => {
 
         expect(1).toBe(1);
     })
+    test('should enter ""', () => {
+        const mommifier = new Mommifier("");
+
+        expect("").toBe(mommifier.mommifier());
+    })
+    test('should enter a', () => {
+        const mommifier = new Mommifier("a");
+
+        expect("mommy").toBe(mommifier.mommifier());
+    })
+    test('should enter b', () => {
+        const mommifier = new Mommifier("b");
+
+        expect("b").toBe(mommifier.mommifier());
+    })
+    test('should enter abbbb', () => {
+        const mommifier = new Mommifier("abbbb");
+
+        expect("abbbb").toBe(mommifier.mommifier());
+    })
+    test('should enter abc', () => {
+        const mommifier = new Mommifier("abc");
+
+        expect("mommybc").toBe(mommifier.mommifier());
+    })
+    test('should enter aeuio', () => {
+        const mommifier = new Mommifier("aeuio");
+
+        expect("mommy").toBe(mommifier.mommifier());
+    })
 })
